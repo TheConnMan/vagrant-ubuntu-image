f46cb0fe744ecbe7170ed88023cc2c0797fa9b94
TheConnMan <bcconn2112@gmail.com>
2015-05-01 23:11:34 -0400

    Added .gitconfig to git.sh.

--- git.sh
+++ git.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get install -y git
+git clone https://gist.github.com/b6af664e23156669afa7.git
+mv b6af664e23156669afa7/.gitconfig /home/vagrant
+chown vagrant:vagrant /home/vagrant/.gitconfig
+rm -rf b6af664e23156669afa7
