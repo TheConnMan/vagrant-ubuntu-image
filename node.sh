260d92204004001bc36c5d191f6b4b7e335d8f38
TheConnMan <bcconn2112@gmail.com>
2015-05-24 18:39:19 -0400

    Added npm latest install.

--- node.sh
+++ node.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get install -y nodejs nodejs-legacy npm
+npm install -g npm@latest
+npm set init.author.name "theconnman"
+npm set init.author.email "bcconn2112@gmail.com"
+npm set init.author.url "http://theconnman.com"
