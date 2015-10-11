7f9c4ee4a0907387c8f1e21f3f12910dfb319f76
TheConnMan <bcconn2112@gmail.com>
2015-10-11 18:57:36 -0400

    Updated node installation script.

--- shell/node.sh
+++ shell/node.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-apt-get install -y nodejs nodejs-legacy npm
+curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+apt-get install -y nodejs
 npm install -g npm@latest
 npm set init.author.name "theconnman"
 npm set init.author.email "bcconn2112@gmail.com"
